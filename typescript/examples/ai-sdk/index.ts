fda44f99c2e9b91379fa506566202eb2516fcdf8
sraj <sraj@paypal.com>
2025-03-30 19:56:07 -0700

    Added an example file

---
+++ typescript/examples/ai-sdk/index.ts
@@ -0,0 +1,93 @@
+import { config } from '@dotenvx/dotenvx'
+import { generateText, LanguageModelV1 } from 'ai';
+// @ts-ignore
+import { Workflows, Toolkit } from '@paypal/agent-toolkit/ai-sdk';
+import * as readline from "readline";
+
+// Get the env file path from an environment variable, with a default fallback
+const envFilePath = process.env.ENV_FILE_PATH;
+config({ path: envFilePath });
+
+/*
+ * This is the merchant's typical use case. This stays the same for most requests.
+ */
+const systemPrompt = `I am a plumber running a small business. I charge $120 per hour plus 50% tax. I use standard parts which typically include a new faucet costing between $50-80 and pipes for about $3 per foot. There is 12% tax for parts. My return URL is: http://localhost:3000/thank-you.`
+
+const agentLog = console.log;
+let agent_prefix = `Agent: `;
+let user_prefix = `You: `;
+
+/*
+ * This holds all the configuration required for starting PayPal Agent Toolkit
+ */
+
+const ppConfig = {
+    clientId: process.env.PAYPAL_CLIENT_ID || '',
+    clientSecret: process.env.PAYPAL_CLIENT_SECRET || '',
+    environment: process.env.PAYPAL_ENVIRONMENT || 'Sandbox',
+    logRequestDetails: process.env.PAYPAL_LOG_REQUESTS || 'false',
+    logResponseDetails: process.env.PAYPAL_LOG_RESPONSES || 'false',
+    debug: process.env.PAYPAL_DEBUG_FLOWS || 'false',
+    logger: console.log
+}
+/*
+ * This holds all the tools that use PayPal functionality
+ */
+const paypalToolkit = new Toolkit(ppConfig);
+
+const paypalWorkflows = new Workflows(ppConfig)
+
+/*
+ * Merchant needs to track the orders on their side so that they can retrieve orderID for future processing
+ */
+let tracker // Add the logic for tracking orders;
+
+let modelInstance// User can bring their own models;
+let llm: LanguageModelV1 = modelInstance.getModel();
+
+const main = async () => {
+    // Start the Chatbot
+    const welcomeMsg = (`\nHow can I help you today? You can enter details to create an order\n[type 'exit' to stop.]\n\n`);
+    chat(welcomeMsg);
+}
+
+async function getInfo(summary: string, value: string) {
+    const { text: result } = await generateText({
+        model: llm,
+        prompt: `Extract ${value} from the prompt: ${summary}. Return only the extracted value.`,
+    });
+    return result;
+}
+
+const chat = (message: string) => {
+    // Simple readline integration
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+
+    rl.question(message, async (userPrompt: string) => {
+        if (userPrompt.toLowerCase() === "exit") {
+            agentLog(`${agent_prefix}Goodbye!`);
+            rl.close();
+            return;
+        }
+
+        try {
+            if (userPrompt.toLowerCase().includes('order')) {
+                agentLog(`${agent_prefix}Sure! Let me do that in steps.`)
+                const customerName = await getInfo(userPrompt, 'customer name');
+                const orderSummary = await paypalWorkflows.generateOrder(llm, userPrompt, systemPrompt);
+                const orderId = await getInfo(orderSummary, 'orderId');
+                tracker.addOrder(customerName, orderId);
+                agentLog(orderSummary);
+            }
+        } catch (error) {
+            console.error('Error generating text:', error);
+        }
+
+        chat(`\n${user_prefix}`); // Loop again for continuous conversation
+    });
+};
+
+main();
