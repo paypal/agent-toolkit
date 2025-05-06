c75b8215174aeac0ac310adb410b0a6ec86e0d27
Harshveer Saini <harsh.s90@gmail.com>
2025-05-05 21:53:40 -0700

    Added template project from spring-initializer to demonstrate integration of a springboot based backend app with Paypal's MCP server

---
+++ java/mcphost/src/main/java/com/paypal/mcphost/McphostApplication.java
@@ -0,0 +1,13 @@
+package com.paypal.mcphost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McphostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McphostApplication.class, args);
+	}
+
+}
