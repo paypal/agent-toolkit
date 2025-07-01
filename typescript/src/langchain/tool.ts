7a5fc1a1ad0b6d059b1ddab2901b681b015bb742
jxnxthxnz <70077577+jxnxthxnz@users.noreply.github.com>
2025-07-01 14:23:56 -0700

    Implement Support for LangChain and OpenAI In TypeScript (#53)

---
+++ typescript/src/langchain/tool.ts
@@ -0,0 +1,38 @@
+import { z } from 'zod';
+import { StructuredTool } from '@langchain/core/tools';
+import {CallbackManagerForToolRun} from '@langchain/core/callbacks/manager';
+import { RunnableConfig } from '@langchain/core/runnables';
+import PayPalAPI from '../shared/api';
+
+class PayPalTool extends StructuredTool {
+  PayPalAPI: PayPalAPI;
+  method: string;
+  name: string;
+  description: string;
+  schema: z.ZodObject<any, any, any, any>;
+
+  constructor(
+    PayPalAPI: PayPalAPI,
+    method: string, 
+    name: string,
+    description: string,
+    schema: z.ZodObject<any, any, any, any, {[x: string]: any}>
+  ) {
+    super();
+    this.PayPalAPI = PayPalAPI;
+    this.method = method;
+    this.name = method;
+    this.description = description;
+    this.schema = schema;
+  }
+
+  _call(
+    arg: z.output<typeof this.schema>,
+    _runManager?: CallbackManagerForToolRun,
+    _parentConfig?: RunnableConfig,
+  ): Promise<any> {
+    return this.PayPalAPI.run(this.method, arg);
+  }
+}
+
+export default PayPalTool;
--- typescript/tsup.config.ts
+++ typescript/tsup.config.ts
@@ -18,5 +18,19 @@ export default defineConfig([{ // Define multiple entry points
     format: ['cjs', 'esm'],
     sourcemap: true,
     target: 'node18',
+},
+{
+    entry: ['src/langchain/index.ts'],
+    outDir: "langchain",
+    dts: true,
+    format: ['cjs', 'esm'],
+    sourcemap: true,
+},
+{
+    entry: ['src/openai/index.ts'],
+    outDir: "openai",
+    dts: true,
+    format: ['cjs', 'esm'],
+    sourcemap: true,
 }
 ]);
